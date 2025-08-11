d309d2c64b39473fb56a3af1ccf0abae3ac012f4
Ishwar Chaudhary <ishwarura@gmail.com>
2025-08-11 22:12:39 +0545

    practice

---
+++ var_let_cosnt.js
@@ -0,0 +1,21 @@
+console.log("JavaScript Practice : var, let and const")
+
+let a = 45;
+let b = "Ishu";
+const author = "Ishwar";
+// author = 5; // Will throw error because constant cannot be changed
+const ishwar = 99;
+let c = null;
+let d = undefined
+
+{
+    let b = 'this'
+    console.log(b)
+}
+
+console.log(b)
+
+console.log(author)
+
+console.log(ishwar)
+
---
+++ variables.js
@@ -0,0 +1,21 @@
+console.log("This is just test")
+
+let a = 69 // a contains 69 (literal = constant value)
+
+console.log(a)
+a = "Ishwar"
+console.log(a)
+
+let ishwar = 7 
+
+console.log(ishwar)
+
+//Reserved word cant be used
+//let var = 88
+
+//JavaScript is casesensitive
+
+// Ishwar and ishwar are different variables (case sensitive)
+
+
+
