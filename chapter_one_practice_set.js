dcb829976cb49d8197c3df0e2345c703ce67a51f
Ishwar Chaudhary <ishwarura@gmail.com>
2025-08-12 21:12:46 +0545

    Create chapter_one_practice_set.js

---
+++ chapter_one_practice_set.js
@@ -0,0 +1,40 @@
+// Create a variable of type string and try to add a number to it.
+
+let h = "Ishwar"
+
+let num = 6
+
+console.log(h + num)
+
+// Use typeof operator to find the datatype of the string in last question
+
+console.log(typeof h, typeof num)
+
+// Create a const object in javascript can you chage it to hold a number later?
+
+const a1 = {
+    name: "Ishwar",
+    section: "One",
+    isPrinciple: false
+}
+
+// a1 = 44
+/// Try to add a new key to the const object in Problem 3
+a1['friend'] = "Subham"
+a1['name'] = "Hell Boy"
+
+console.log(a1)
+
+// Write a js program to create a word-meaning dictonary of 5 words.
+
+const dict = {
+    appreciate: "recognize the full worth of.",
+    ataraxia: "state of freedom from emotional......",
+    yakka: "hard work",
+    cat: "catttt",
+    smile: "smile"
+}
+
+console.log(dict.yakka)
+
+
