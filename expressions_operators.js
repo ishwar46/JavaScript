0d94cf546e1fd8333c18287c5ca8e554974d0e0f
Ishwar Chaudhary <ishwarura@gmail.com>
2025-08-18 17:24:11 +0545

    Create expressions_operators.js

---
+++ expressions_operators.js
@@ -0,0 +1,67 @@
+// 77;
+
+// "Ishwar Chaudhary"
+
+// false
+
+console.log("Operators in Js")
+// Arithmetic Operators
+let a = 10;
+let b = 4;
+
+// console.log("a + b = ", a + b)
+// console.log("a - b = ", a - b)
+// console.log("a / b = ", a / b)
+// console.log("a ** b = ", a ** b)
+// console.log("a % b = ", a % b)
+// console.log("++a = ", ++a)
+// console.log("a++ = ", a++)
+// console.log("--a = ", --a)
+// console.log("a-- = ", a--)
+// console.log("a = ", a)
+// console.log("a-- = ", a--)
+
+
+// Assigment Operators
+let assigment = 1
+assigment += 5 // same as a = a + 9
+console.log("a is now = ", a)
+
+assigment -= 5 // smae as assignment = assignment - 5
+console.log("a is now =", a)
+
+assigment *= 5 // same as assignment = assigment * 5
+console.log("a is now = ", a)
+
+assigment /= 5 // same as assigment = assigment / 5
+console.log("a is now = ", a)
+
+// Comparison Operators
+
+let comp1 = 6
+let comp2 = 6
+console.log("comp1 == comp2 is ", comp1 == comp2)
+console.log("comp1 == comp2 is ", comp1 != comp2)
+console.log("comp1 == comp2 is ", comp1 === comp2)
+console.log("comp 1 == comp2 is", comp1 !== comp2)
+console.log("comp1 > comp2 is", comp1 > comp2)
+
+//Logical Operator
+
+let x = 5;
+let y = 6;
+
+console.log("this is ", x < y && x == 5)
+console.log("this is ", x > y || x == 5)
+console.log(!false)
+console.log(!true)
+
+//Comments
+
+//Single line comments
+
+/* Multi Line
+Multi Line Commments
+Comments*/
+
+
